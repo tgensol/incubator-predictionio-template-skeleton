91ea3907506aafb44b58c9e703a9263cda5758e4
Thibaut Gensollen <thibaut.gensollen@ens-cachan.fr>
2017-12-05 14:52:36 +0100

    test

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -2,17 +2,18 @@ package org.template.classification
 
 import org.apache.predictionio.controller.PDataSource
 import org.apache.predictionio.controller.EmptyEvaluationInfo
-import org.apache.predictionio.controller.EmptyActualResult
 import org.apache.predictionio.controller.Params
 import org.apache.predictionio.data.storage.Event
 import org.apache.predictionio.data.store.PEventStore
 
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 
 import grizzled.slf4j.Logger
 
-case class DataSourceParams(appName: String) extends Params
+case class DataSourceParams(val appName: String) extends Params
+
 class DataSource(val dsp: DataSourceParams)
   extends PDataSource[TrainingData,
       EmptyEvaluationInfo, Query, ActualResult] {
